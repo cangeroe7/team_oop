069ee6279726590ff9cda246c06e785d6c1a04bb
cangeroe7 <105395969+cangeroe7@users.noreply.github.com>
2023-09-06 09:36:12 -0500

    Added basic structure

---
+++ team_oop/classes.py
@@ -0,0 +1,9 @@
+class Game:
+    def __init__(self, games):
+        self.games = games
+        self.player1 = ""
+        self.player2 =  ""
+        self.player1Level = 0
+        self.player2Level = 0
+
+    
---
+++ team_oop/games.py
@@ -0,0 +1,28 @@
+games = []
+def game1():
+    print("game 1")
+games.append(game1)
+
+def game2():
+    print("game 2")
+games.append(game2)
+
+def game3():
+    print("game 3")
+games.append(game3)
+
+def game4():
+    print("game 4")
+games.append(game4)
+
+def game5():
+    print("game 5")
+games.append(game5)
+
+def game6():
+    print("game 6")
+games.append(game6)
+
+def game7():
+    print("game 7")
+games.append(game7)
